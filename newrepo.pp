ae153338079fc206df3b6f889d97adee5cfae005
Vagrant Default User <vagrant@stretch.localdomain>
2017-11-19 14:46:53 +0000

    create new repository on command line

---
+++ newrepo.pp
@@ -0,0 +1,9 @@
+#…or create a new repository on the command line
+
+#echo "# ictnetwork2017" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git remote add origin https://github.com/natnael8/ictnetwork2017.git
+#git push -u origin master
+
