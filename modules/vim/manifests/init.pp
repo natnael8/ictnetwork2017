8508d9f5c51d811b63eeb0c5268907c7eb7a26ba
root <root@stretch.localdomain>
2017-11-22 10:08:10 +0000

    install vim

---
+++ modules/vim/manifests/init.pp
@@ -0,0 +1,10 @@
+class vim {
+     exec { 'apt-get update':
+       command => '/usr/bin/apt-get update'
+      }
+
+     package { 'vim':
+       ensure => 'installed'
+     require => Exec['apt-get update']
+     }
+}
