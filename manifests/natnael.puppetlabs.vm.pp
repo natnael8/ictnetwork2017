cb6ef1431c7f9c22ede5cc37e1dcda9e7fa4f673
NATNAEL H <natnael.hailemichael.ju.edu.et>
2017-11-26 22:17:51 +0100

    create ldap server

--- manifests/natnael.puppetlabs.vm.pp
+++ manifests/natnael.puppetlabs.vm.pp
@@ -1,12 +1,21 @@
 node default {
-   class { 'ntp':
-
-   }
-
-}
-
-  node 'natnael.puppetlabs.vm' {
-	include apache
-  #include systemsusers
-  #include vim
+     class { 'ntp':
+      }
+}	
+node 'natnael.puppetlabs.vm' {
+#	include apache
+#  	include systemsusers
+#  	include vim
+#	include ntp
+  class { 'openldap::server': 
+    openldap::server::database { 'dc=juldap,dc=example.com':
+  	ensure => present,
+        }
+        provider => 'augeas',
+    openldap::server::database { 'dc=ju,dc=edu,dc=et':
+        directory => '/var/lib/ldap',
+        rootdn    => 'cn=admin,dc=ju,dc=edu,dc=et',
+        rootpw    => 'secret',
+        }
+        }
 }
