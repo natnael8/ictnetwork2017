967221b145cc412dc1e2e7fa944b14d99a626b6c
root <root@stretch.localdomain>
2017-11-27 13:29:45 +0000

    adding ntp module and manage NTP client via puppet

--- manifests/stretch.pp
+++ manifests/stretch.pp
@@ -1,4 +1,9 @@
 node 'stretch'{
+  
+  class { 'ntp':
+    server_list => [ 'mail.ju.edu.et','0.be.pool.ntp.org' ],
+  }
+
   class { 'resolv_conf':
     nameservers => ['10.140.5.25', '10.0.2.3'],
     searchpath  => ['ju.edu.et', 'wireless.UGent.be'],
