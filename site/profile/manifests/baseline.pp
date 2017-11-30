fd6f5633da42f0ac547fd0b04699282e470b6dc0
root <root@stretch.localdomain>
2017-11-30 08:40:18 +0000

    editing baseline

--- site/profile/manifests/baseline.pp
+++ site/profile/manifests/baseline.pp
@@ -14,4 +14,38 @@ class profile::baseline {
     uri        => ['ldap://ldap.ju.edu.et', 'ldap://ldap-master.ju.edu.et:666'],
   }
 
+  apt::source { 'puppetlabs':
+    location => 'http://apt.puppetlabs.com',
+    repos    => 'main',
+    key      => {
+      'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
+      'server' => 'pgp.mit.edu',
+     },
+   }
+
+   if($facts[operatingsystem] == 'Debian') {
+    apt::source { 'debian':
+      ensure   => 'present',
+      location => 'http://ftp.be.debian.org/debian',
+      repos    => 'main non-free contrib',
+     }
+   }
+
+   if($facts[operatingsystem] == 'Ubuntu') {
+    apt::source { 'ubuntu':
+      ensure   => 'present',
+      location => 'http://be.archive.ubuntu.com/ubuntu/',
+      repos    => 'main restricted universe multiverse ',
+     }
+    }
+
+
+   accounts::user { 'natyh':
+    uid      => '5012',
+    gid      => '4001',
+    group    => 'admin',
+    shell    => '/bin/bash',
+    password => 'Asdf1234',
+    locked   => false,
+   }
 }
